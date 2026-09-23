843ef589a2567de3b4ca466c1e88e44c633aaf15
freqmc <nickit.birukoff@yandex.ru>
2026-09-24 00:52:35 +0300

    f

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"DataWriter"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"NewCurrData"},{"p":"org.example","l":"Utilities"}];updateSearchResults();
